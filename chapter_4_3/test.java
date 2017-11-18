db13646b8b58c9cb152b0047ed44bb115ff35bbe
wenjiewang98228 <33054589+wenjiewang98228@users.noreply.github.com>
2017-11-18 17:17:39 +0800

    Add files via upload

---
+++ chapter_4_3/test.java
@@ -0,0 +1,19 @@
+package chapter_4_3;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int absI = -2;
+		int value = Math.abs(absI);
+		System.out.println(value);
+		
+		float abdF = -3.141F;
+		System.out.println(Math.abs(abdF));
+		
+		double absL = -3.146912135;
+		System.out.println(Math.abs(absL));
+	}
+
+}
